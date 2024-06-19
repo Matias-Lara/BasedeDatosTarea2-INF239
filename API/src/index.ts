a59a5ca1c13ec12423660edc613a858639622341
holyusm <130817064+holyusm@users.noreply.github.com>
2024-06-19 00:41:50 -0400

    Endpoint registrar

--- API/src/index.ts
+++ API/src/index.ts
@@ -5,6 +5,37 @@ import {PrismaClient} from "@prisma/client";
 const prisma = new PrismaClient();
 const app = new Elysia();
 
+interface User {
+    id?:          number
+    nombre:       string
+    correo:       string   
+    clave:        string
+    descripcion?: string
+}
+
+
+app.get('/api/registrar', () => {
+    return prisma.usuario.findMany()
+})
+
+
+app.post('/api/registrar', async ({body}) => {
+    try{
+        const {nombre, correo, clave} = await body as User;
+        const newUser = await prisma.usuario.create({data: body as User});
+        return{
+            estado: 201,
+            mensaje: `El correo se ha registrado correctamente`
+        };
+        
+    } catch (err){
+        console.log(`Error al intentar registrar el usuario: ${(err as Error).message}`);
+        return {
+        estado: 500,
+        mensaje: 'Ha existido un error al realizar la peticion'
+        };
+    }
+});
 
 app.listen(3000, () => {
     console.log('🦊 Elysia is running on http://localhost:3000');
